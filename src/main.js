7340ed27d0d9adf65bc044a8afb40b795634ab15
liyang <272891021@qq.com>
2024-07-05 03:19:39 +0800

    change to eromanga.top

--- src/main.js
+++ src/main.js
@@ -13,22 +13,22 @@ document.addEventListener('alpine:init', () => {
             },
         },
         submitForm() {
-            fetch('http://127.0.0.1:5800/api/issue', {
+            fetch('http://eromanga.top:5800/api/issue', {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(this.reswithverifycode)
             })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error('Error:', error));
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error('Error:', error));
         },
 
         getVerifyCode() {
-            fetch('http://127.0.0.1:5800/api/verifycode')
+            fetch('http://eromanga.top:5800/api/verifycode')
                 .then(response => response.text())
-                .then(text => {this.reswithverifycode.verifycode_url = text})
+                .then(text => { this.reswithverifycode.verifycode_url = text })
         }
     }));
 });
